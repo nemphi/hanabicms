897009a0c04f4ba19d14da39dce00363cc56d0e0
Juan Alvarez <juan@nemphi.com>
2023-06-22 16:24:36 -0500

    fix: type error

--- src/api/install.ts
+++ src/api/install.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { C } from ".";
 import { ApiSimpleResponse } from "../lib/types";
 import { nanoid } from "nanoid";
-import { User } from "./users";
+import { User, UserMetadata } from "./users";
 
 
 const app = new Hono<C>();
@@ -26,16 +26,16 @@ app.get("/", async c => {
         salt: "63d84400bbfec879d8089ec7b5f044d49b2fe39b6486bd7b273cbbae26bfb3e85eb622f3b89ccef35ee90f5ea9393fbf6591951cf6d49231ce0bde55258a276e",
         password: "$2a$12$aqv2aPQGAL9kSHteiiYMne8phPK/w6MAukGiXobkP59ONBw/vwpb.", // admin
         roles: "admin",
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString(),
     }
 
-    await c.env.kvCMS.put(`users/${user.id}`, JSON.stringify(user), {
-        metadata: {
-            email: user.email,
-            roles: user.roles
-        }
-    })
+    const metadata: UserMetadata = {
+        email: user.email,
+        roles: user.roles,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    }
+
+    await c.env.kvCMS.put(`users/${user.id}`, JSON.stringify(user), { metadata })
 
     await c.env.kvCMS.put("config/installed", "true")
 
